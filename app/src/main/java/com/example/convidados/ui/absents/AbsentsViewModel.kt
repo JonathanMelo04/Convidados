7e79c92e1e48b7fd2788f05c4878a3ecde0c340c
Jonathan Melo <jonathanmelo04@gmail.com>
2021-11-18 23:54:44 -0300

    Adjustments and changes to ViewModel : Absents, AllGuests and Presents

--- app/src/main/java/com/example/convidados/ui/absents/AbsentsViewModel.kt
+++ app/src/main/java/com/example/convidados/ui/absents/AbsentsViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.convidados.ui.slideshow
+package com.example.convidados.ui.absents
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SlideshowViewModel : ViewModel() {
+class AbsentsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "This is absents Fragment"
     }
     val text: LiveData<String> = _text
 }
