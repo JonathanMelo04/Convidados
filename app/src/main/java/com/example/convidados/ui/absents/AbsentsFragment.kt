162c7afa74a9c59d380dad202d39488e415d78af
Jonathan Melo <jonathanmelo04@gmail.com>
2021-11-18 23:48:16 -0300

    Adjustments and changes in classes: Absents, AllGuests and Presents

--- app/src/main/java/com/example/convidados/ui/absents/AbsentsFragment.kt
+++ app/src/main/java/com/example/convidados/ui/absents/AbsentsFragment.kt
@@ -1,4 +1,4 @@
-package com.example.convidados.ui.gallery
+package com.example.convidados.ui.absents
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,13 +8,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.convidados.R
-import com.example.convidados.databinding.FragmentGalleryBinding
+import com.example.convidados.databinding.FragmentAbsentsBinding
 
-class GalleryFragment : Fragment() {
+class AbsentsFragment : Fragment() {
 
-    private lateinit var galleryViewModel: GalleryViewModel
-    private var _binding: FragmentGalleryBinding? = null
+    private lateinit var absentsViewModel: AbsentsViewModel
+    private var _binding: FragmentAbsentsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,14 +24,14 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        absentsViewModel =
+            ViewModelProvider(this).get(AbsentsViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentAbsentsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner, Observer {
+        val textView: TextView = binding.textAbsents
+        absentsViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
