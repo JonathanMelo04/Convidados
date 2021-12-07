32eaf7628b2ddb4fc92c35f17f1e108dacbabe4f
Jonathan Melo <jonathanmelo04@gmail.com>
2021-12-07 15:43:04 -0300

    Adding methods in GuestFormActivity

--- app/src/main/java/com/example/convidados/GuestFormActivity.kt
+++ app/src/main/java/com/example/convidados/GuestFormActivity.kt
@@ -2,10 +2,36 @@ package com.example.convidados
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import com.example.convidados.databinding.ActivityGuestFormBinding
 
-class GuestFormActivity : AppCompatActivity() {
+class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityGuestFormBinding
+    private lateinit var mViewModel: GuestFormViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_guest_form)
+
+        binding = ActivityGuestFormBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mViewModel = ViewModelProvider(this).get(GuestFormViewModel::class.java)
+
+        setListeners()
+
     }
+
+    private fun setListeners(){
+        binding.buttonSave.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+       val id = v!!.id
+        if(id == R.id.button_save){
+
+        }
+    }
+
 }
