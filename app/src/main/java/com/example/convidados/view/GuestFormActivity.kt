0d9b455ed58bcd11b67a13cfccf6721c9d2a5132
Jonathan Melo <jonathanmelo04@gmail.com>
2021-12-10 17:37:19 -0300

    function observe
    Added
    function observe.
    
    OnClick function reallocation.

--- app/src/main/java/com/example/convidados/view/GuestFormActivity.kt
+++ app/src/main/java/com/example/convidados/view/GuestFormActivity.kt
@@ -3,6 +3,8 @@ package com.example.convidados.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.convidados.viewmodel.GuestFormViewModel
 import com.example.convidados.R
@@ -22,15 +24,11 @@ class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
         mViewModel = ViewModelProvider(this).get(GuestFormViewModel::class.java)
 
         setListeners()
-
-    }
-
-    private fun setListeners(){
-        binding.buttonSave.setOnClickListener(this)
+        observe()
     }
 
     override fun onClick(v: View?) {
-       val id = v!!.id
+        val id = v!!.id
         if(id == R.id.button_save){
 
             val name = binding.editName.text.toString()
@@ -41,4 +39,17 @@ class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    private fun observe(){
+        mViewModel.saveGuest.observe(this, Observer {
+            if(it){
+                Toast.makeText(applicationContext, "Sucesso" , Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(applicationContext, "Falha" , Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun setListeners(){
+        binding.buttonSave.setOnClickListener(this)
+    }
 }
